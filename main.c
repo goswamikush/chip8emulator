31960c53c6b699f420e3e0a392edad36b4c2e277
Kush Goswami <kushgoswami@kushs-air-2.mynetworksettings.com>
2025-12-14 15:51:26 -0500

    Initialize values

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <string.h>
 
 // RAM
 uint8_t chip8ram[4096];
@@ -14,6 +15,8 @@ uint8_t sound_timer;
 // pseudo-registers
 uint16_t pc;
 uint8_t sp;
+
+// Stack
 uint16_t stack[16];
 
 // I register
@@ -21,3 +24,30 @@ uint16_t I;
 
 // Display
 uint8_t display[32][64];
+
+int initialize_values() {
+    // RAM from 0 to 4095
+    memset(chip8ram, 0, sizeof(chip8ram));
+
+    // General purpose registers
+    memset(gp_registers, 0, sizeof(gp_registers));
+
+    // Special purpose registers
+    delay_timer = 0;
+    sound_timer = 0;
+
+    // Pseudo-registers
+    pc = 0x200;
+    sp = 0;
+
+    // Stack
+    memset(stack, 0, sizeof(stack));
+
+    // I register
+    I = 0;
+
+    // Display
+    memset(display, 0, sizeof(display));
+
+    return 0;
+}
